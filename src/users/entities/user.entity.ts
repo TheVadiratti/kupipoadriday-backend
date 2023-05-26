5f78adac417b4c20a0e3721e8ea0815e6ed2b328
Vadim Belousov <thevadiratti@yandex.ru>
2023-05-26 14:03:28 +0500

    feat: добавить схему User

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,42 @@
-export class User {}
+import { Entity, Column, OneToMany } from 'typeorm';
+import { Length, IsNotEmpty, IsUrl, IsEmail } from 'class-validator';
+import { Common } from 'src/app.entity';
+import { Wish } from 'src/wishes/entities/wish.entity';
+import { Offer } from 'src/offers/entities/offer.entity';
+import { Wishlist } from 'src/wishlists/entities/wishlist.entity';
+
+@Entity()
+export class User extends Common {
+  @Column({ unique: true })
+  @Length(2, 30)
+  @IsNotEmpty()
+  username: string;
+
+  @Column({ default: 'Пока ничего не рассказал о себе' })
+  @Length(2, 200)
+  about: string;
+
+  @Column({ default: 'https://i.pravatar.cc/300' })
+  @IsUrl()
+  avatar: string;
+
+  @Column({ unique: true })
+  @IsEmail()
+  email: string;
+
+  @Column()
+  @IsNotEmpty()
+  password: string;
+
+  @Column()
+  @OneToMany(() => Wish, (wish) => wish.owner)
+  wishes: Wish[];
+
+  @Column()
+  @OneToMany(() => Offer, (offer) => offer.user)
+  offers: Offer[];
+
+  @Column()
+  @OneToMany(() => Wishlist, (wishlist) => wishlist.owner)
+  wishlists: Wishlist[];
+}
