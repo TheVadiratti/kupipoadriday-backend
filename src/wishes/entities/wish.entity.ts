0ca870d84e192458383e06f68165eeb238cf4024
Vadim Belousov <thevadiratti@yandex.ru>
2023-05-26 16:05:06 +0500

    feat: добавить схему Wish

--- src/wishes/entities/wish.entity.ts
+++ src/wishes/entities/wish.entity.ts
@@ -1,1 +1,41 @@
-export class Wish {}
+import { Entity, Column, ManyToOne, OneToMany } from 'typeorm';
+import { Length, IsUrl } from 'class-validator';
+import { Common } from 'src/app.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Offer } from 'src/offers/entities/offer.entity';
+
+@Entity()
+export class Wish extends Common {
+  @Column()
+  @Length(1, 200)
+  name: string;
+
+  @Column()
+  @IsUrl()
+  link: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+
+  @Column({ type: 'decimal', scale: 2 })
+  price: number;
+
+  @Column({ type: 'decimal', scale: 2 })
+  reised: number;
+
+  @Column()
+  @ManyToOne(() => User, (user) => user.wishes)
+  owner: User;
+
+  @Column()
+  @Length(1, 1024)
+  description: string;
+
+  @Column()
+  @OneToMany(() => Offer, (offer) => offer.item)
+  offers: Offer[];
+
+  @Column({ default: 0 })
+  copied: number;
+}
