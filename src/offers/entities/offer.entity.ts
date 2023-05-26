1bbc1d8bcf7c757653abe1f20de0a732eb3be7c9
Vadim Belousov <thevadiratti@yandex.ru>
2023-05-26 16:45:18 +0500

    feat: добавить схему Offer

--- src/offers/entities/offer.entity.ts
+++ src/offers/entities/offer.entity.ts
@@ -1,1 +1,21 @@
-export class Offer {}
+import { Entity, Column, ManyToOne } from 'typeorm';
+import { Common } from 'src/app.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Wish } from 'src/wishes/entities/wish.entity';
+
+@Entity()
+export class Offer extends Common {
+  @Column()
+  @ManyToOne(() => User, (user) => user.offers)
+  user: User;
+
+  @Column()
+  @ManyToOne(() => Wish, (wish) => wish.offers)
+  item: Wish;
+
+  @Column({ type: 'decimal', scale: 2 })
+  amount: number;
+
+  @Column({ default: false })
+  hidden: boolean;
+}
