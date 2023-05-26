c2cd2b02ac6168ba3cad269e6484172fa1ec9644
Vadim Belousov <thevadiratti@yandex.ru>
2023-05-26 16:23:05 +0500

    feat: добавить схему Wishlist

--- src/wishlists/entities/wishlist.entity.ts
+++ src/wishlists/entities/wishlist.entity.ts
@@ -1,1 +1,24 @@
-export class Wishlist {}
+import { Entity, Column, ManyToMany, JoinTable } from 'typeorm';
+import { IsUrl, Length, MaxLength } from 'class-validator';
+import { Common } from 'src/app.entity';
+import { Wish } from 'src/wishes/entities/wish.entity';
+
+@Entity()
+export class Wishlist extends Common {
+  @Column()
+  @Length(1, 250)
+  name: string;
+
+  @Column()
+  @MaxLength(1500)
+  description: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+
+  @Column()
+  @ManyToMany(() => Wish)
+  @JoinTable()
+  items: Wish[];
+}
