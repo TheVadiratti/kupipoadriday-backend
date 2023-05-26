903eac793ba35f8824826227045c19e4be704acf
Vadim Belousov <thevadiratti@yandex.ru>
2023-05-26 16:51:37 +0500

    feat: добавить создателя вишлиста в схему

--- src/wishlists/entities/wishlist.entity.ts
+++ src/wishlists/entities/wishlist.entity.ts
@@ -1,10 +1,15 @@
-import { Entity, Column, ManyToMany, JoinTable } from 'typeorm';
+import { Entity, Column, ManyToMany, JoinTable, ManyToOne } from 'typeorm';
 import { IsUrl, Length, MaxLength } from 'class-validator';
 import { Common } from 'src/app.entity';
 import { Wish } from 'src/wishes/entities/wish.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Entity()
 export class Wishlist extends Common {
+  @Column()
+  @ManyToOne(() => User, (user) => user.wishlists)
+  owner: User;
+
   @Column()
   @Length(1, 250)
   name: string;
