f49e726d8f2178b6ff2c1d3c413445be925f7aa6
Vadim Belousov <thevadiratti@yandex.ru>
2023-06-02 13:15:41 +0500

    feat: описать wish dto

--- src/wishes/dto/create-wish.dto.ts
+++ src/wishes/dto/create-wish.dto.ts
@@ -1,1 +1,18 @@
-export class CreateWishDto {}
+import { Length, IsString, IsUrl, Min } from 'class-validator';
+
+export class CreateWishDto {
+  @Length(1, 250)
+  name: string;
+
+  @IsUrl()
+  link: string;
+
+  @IsUrl()
+  image: string;
+
+  @Min(1)
+  price: number;
+
+  @IsString()
+  description: string;
+}
