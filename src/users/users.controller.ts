fea8099d47ab2956696bc677bf6cf65eb3f47b7b
Vadim Belousov <thevadiratti@yandex.ru>
2023-06-02 12:19:25 +0500

    feat: добавить контроллеры для редактирования и просмотра профиля

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,18 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Patch,
+  UseGuards,
+  Req,
+  Param,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtGuard } from '../auth/auth.guard';
+import { AuthRequest } from '../types';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +21,30 @@ export class UsersController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('me')
+  findOwn(@Req() req: AuthRequest) {
+    return this.usersService.findOne({
+      where: { username: req.user.username },
+    });
+  }
+
+  @UseGuards(JwtGuard)
+  @Get(':username')
+  findOne(@Param('username') username: string) {
+    return this.usersService.findOne({ where: { username } });
+  }
+
+  @UseGuards(JwtGuard)
+  @Patch('me')
+  update(@Req() req: AuthRequest, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.updateOne(
+      {
+        where: { username: req.user.username },
+      },
+      updateUserDto,
+    );
   }
 }
