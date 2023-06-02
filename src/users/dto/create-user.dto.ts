ad4a52bda6d3e2092334954f1eb5c7af3df3c716
Vadim Belousov <thevadiratti@yandex.ru>
2023-06-02 11:15:10 +0500

    fix: исправить CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,12 @@
-import { IsEmail, IsNotEmpty, IsString, IsUrl } from 'class-validator';
+import { Length, IsEmail, IsNotEmpty, IsString, IsUrl } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @Length(1, 64)
   username: string;
 
   @IsString()
+  @Length(0, 200)
   about: string;
 
   @IsUrl()
