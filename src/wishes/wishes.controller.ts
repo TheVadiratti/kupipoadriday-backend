5c612e00244a8db0ec846bab2db31661299a1a11
Vadim Belousov <thevadiratti@yandex.ru>
2023-06-02 13:51:19 +0500

    feat: реализовать CRUD контроллеры wishes

--- src/wishes/wishes.controller.ts
+++ src/wishes/wishes.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { WishesService } from './wishes.service';
 import { CreateWishDto } from './dto/create-wish.dto';
+import { UpdateWishDto } from './dto/update-wish.dto';
 
 @Controller('wishes')
 export class WishesController {
@@ -9,5 +18,20 @@ export class WishesController {
   @Post()
   create(@Body() createWishDto: CreateWishDto) {
     return this.wishesService.create(createWishDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.wishesService.findOne({ where: { id } });
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() updateWishDto: UpdateWishDto) {
+    return this.wishesService.updateOne({ where: { id } }, updateWishDto);
+  }
+
+  @Delete('id')
+  removeOne(@Param('id') id: number) {
+    return this.wishesService.removeOne({ where: { id } });
   }
 }
