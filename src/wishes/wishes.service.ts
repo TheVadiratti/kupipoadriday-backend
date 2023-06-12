d403c1711c98703b8112ef84454028224aaeb04f
Vadim Belousov <thevadiratti@yandex.ru>
2023-06-12 13:05:12 +0500

    refactor: переделать сервисы wishes под транзакции

--- src/wishes/wishes.service.ts
+++ src/wishes/wishes.service.ts
@@ -12,6 +12,7 @@ import {
   FindOptionsWhere,
   UpdateResult,
   FindManyOptions,
+  DataSource,
 } from 'typeorm';
 import { Wish } from './entities/wish.entity';
 import { User } from '../users/entities/user.entity';
@@ -22,6 +23,7 @@ export class WishesService {
   constructor(
     @InjectRepository(Wish)
     private wishRepository: Repository<Wish>,
+    private dataSource: DataSource,
   ) {}
 
   create(createWishDto: CreateWishDto, owner: User): Promise<Wish> {
@@ -42,22 +44,38 @@ export class WishesService {
     updateWishDto: UpdateWishDto,
     user: User,
   ): Promise<UpdateResult> {
-    const wish = await this.findOne({
-      where: query,
-      relations: { owner: true },
-    });
+    const queryRunner = this.dataSource.createQueryRunner();
 
-    if (wish.owner.username !== user.username) {
-      throw new ForbiddenException('Запрещено менять чужие хотелки.');
-    }
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const wish = await this.findOne({
+        where: query,
+        relations: { owner: true },
+      });
 
-    if (updateWishDto.price && wish.raised > 0) {
-      throw new ForbiddenException(
-        'Запрещено менять стоимость подарка, если уже есть скинувшиеся.',
+      if (wish.owner.username !== user.username) {
+        throw new ForbiddenException('Запрещено менять чужие хотелки.');
+      }
+
+      if (updateWishDto.price && wish.raised > 0) {
+        throw new ForbiddenException(
+          'Запрещено менять стоимость подарка, если уже есть скинувшиеся.',
+        );
+      }
+
+      const updatedWish = await this.wishRepository.update(
+        query,
+        updateWishDto,
       );
+      await queryRunner.commitTransaction();
+      return updatedWish;
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    } finally {
+      await queryRunner.release();
     }
-
-    return this.wishRepository.update(query, updateWishDto);
   }
 
   async updateRaised(
@@ -65,49 +83,98 @@ export class WishesService {
     user: User,
     amount: number,
   ): Promise<UpdateResult> {
-    const wish = await this.findOne({
-      where: { id },
-      relations: { owner: true },
-    });
+    const queryRunner = this.dataSource.createQueryRunner();
 
-    if (user.username === wish.owner.username) {
-      throw new ForbiddenException('Нельзя скидываться на подарок себе.');
-    }
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-    const newRaised = getReised(Number(wish.raised), amount);
-    if (wish.price < newRaised) {
-      throw new BadRequestException(
-        'Сумма собранных средств не может превышать стоимость подарка.',
+    try {
+      const wish = await this.findOne({
+        where: { id },
+        relations: { owner: true },
+      });
+
+      if (user.username === wish.owner.username) {
+        throw new ForbiddenException('Нельзя скидываться на подарок себе.');
+      }
+
+      const newRaised = getReised(Number(wish.raised), amount);
+      if (wish.price < newRaised) {
+        throw new BadRequestException(
+          'Сумма собранных средств не может превышать стоимость подарка.',
+        );
+      }
+
+      const updatedWish = await this.wishRepository.update(
+        { id },
+        { raised: newRaised },
       );
-    }
 
-    return this.wishRepository.update({ id }, { raised: newRaised });
+      await queryRunner.commitTransaction();
+      return updatedWish;
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async makeCopy(id: number, user: User): Promise<Wish> {
-    const wish = await this.findOne({ where: { id } });
-    const { name, link, image, price, description, copied } = wish;
-    await this.wishRepository.update({ id }, { copied: copied + 1 });
-    return this.create({ name, link, image, price, description }, user);
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const wish = await this.findOne({ where: { id } });
+      const { name, link, image, price, description, copied } = wish;
+      await this.wishRepository.update({ id }, { copied: copied + 1 });
+      const copy = await this.create(
+        { name, link, image, price, description },
+        user,
+      );
+
+      await queryRunner.commitTransaction();
+      return copy;
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async removeOne(query: FindOptionsWhere<Wish>, user: User): Promise<Wish> {
-    const wish = await this.findOne({
-      where: query,
-      relations: {
-        owner: true,
-        offers: {
-          user: true,
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const wish = await this.findOne({
+        where: query,
+        relations: {
+          owner: true,
+          offers: {
+            user: true,
+          },
         },
-      },
-    });
+      });
 
-    if (wish.owner.username !== user.username) {
-      throw new ForbiddenException('Нельзя удалять чужие хотелки.');
+      if (wish.owner.username !== user.username) {
+        throw new ForbiddenException('Нельзя удалять чужие хотелки.');
+      }
+
+      await this.wishRepository.delete(query);
+
+      await queryRunner.commitTransaction();
+      return wish;
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    } finally {
+      await queryRunner.release();
     }
-
-    await this.wishRepository.delete(query);
-
-    return wish;
   }
 }
